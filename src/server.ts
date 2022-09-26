84af7cadabccbc678480da2f61091e20bff49459
RMidhunSuresh <hi@midhun.dev>
2022-09-26 12:39:15 +0530

    Remove test code

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import { NetworkProxyTrafficLightClient } from "./trafficlight";
 
-async function doFoo() {
-    const SYNAPSE_HOST = "http://localhost:35355";
-    const trafficLightClient = new NetworkProxyTrafficLightClient("http://localhost:5000", "http://localhost:4040");
-    trafficLightClient["createProxy"]("http://172.18.0.5:8008");
-    trafficLightClient["disableEndpoint"]("/_matrix/static/");
-    // await trafficLightClient.register();
-    // await trafficLightClient.start();
+const proxyURL = process.env.PROXY_URL ?? "http://localhost:4040";
+const trafficlightURL = process.env.trafficlightURL ?? "http://localhost:5000";
+
+async function start() {
+    const trafficLightClient = new NetworkProxyTrafficLightClient(trafficlightURL, proxyURL);
+    await trafficLightClient.register();
+    await trafficLightClient.start();
 }
 
-doFoo();
+start();
