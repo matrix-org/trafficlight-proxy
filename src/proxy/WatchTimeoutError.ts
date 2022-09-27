9561eed0128840bd4ce512ead43a7d480386cdc6
RMidhunSuresh <hi@midhun.dev>
2022-09-28 00:14:20 +0530

    Custom error

---
+++ src/proxy/WatchTimeoutError.ts
@@ -0,0 +1,10 @@
+export class WatchTimeoutError extends Error {
+
+    get name() {
+        return "WatchTimeoutError";
+    }
+
+    get message() {
+        return "Endpoint was not accessed before timeout.";
+    }
+}
